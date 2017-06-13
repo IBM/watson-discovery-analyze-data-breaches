0d6339b870f11d824ef811cf8d932b30b6a3237a
Drew Neil <andrew.jr.neil@gmail.com>
2017-06-13 17:03:12 +0100

    Simplify the NoResults component

--- views/NoResults/index.jsx
+++ views/NoResults/index.jsx
@@ -2,19 +2,7 @@ import React, { PropTypes } from 'react';
 
 const NoResults = (props) => (
   <div style={{ width: '100%', textAlign: 'center', marginBottom: '5rem' }}>
-  <p className="base--h2">There are no results for:</p>
-    <div className="query--no-results-wrapper">
-      <div className="query--no-results base--h2">
-        {props.query ? props.query.text : '' }
-      </div>
-    </div>
-    <p className='base--h3'>
-    {props.query.restrictedDateRange ? (
-      "Try expanding the timeframe."
-    ) : (
-      "Try modifying your query."
-    )}
-    </p>
+    <p className="base--h2">There are no results.</p>
   </div>
 );
 
