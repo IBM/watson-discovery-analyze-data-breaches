631d8340ccac9be3bf423e9a2638e968c977e96a
Drew Neil <andrew.jr.neil@gmail.com>
2017-06-14 18:39:16 +0100

    Show extracted entities for each result

--- views/ResultsList/Detail.jsx
+++ views/ResultsList/Detail.jsx
@@ -7,16 +7,21 @@ export default React.createClass({
   },
 
   render() {
+    let item          = this.props.item || {};
+    let text          = item.text || '';
+    let enriched_text = item.enriched_text || {};
+    let entities      = enriched_text.entities || [];
+
     return (
       <div className="widget--detail">
         <h3>{this.props.item.title}</h3>
         <div className="widget--text">
-          <p>
-            {this.props.item.text}
-          </p>
+          <p>{text}</p>
         </div>
         <div className="widget--entities">
-          Extracted Entities
+          {entities.map((entity) =>
+            <div>{entity.text} ({entity.type})</div>
+          )}
         </div>
       </div>
     );
