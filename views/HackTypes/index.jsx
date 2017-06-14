532b37bd637b5724af379b1b4dc45d588e0fdab3
Drew Neil <andrew.jr.neil@gmail.com>
2017-06-14 12:45:07 +0100

    Put the "What type of breach?" select menu in a purple row

--- views/HackTypes/index.jsx
+++ views/HackTypes/index.jsx
@@ -24,24 +24,32 @@ export default React.createClass({
 
   render() {
     return (
-      <div>
-        <h2>HackTypes</h2>
-        <select
-          onChange={this.handleChange}
-          value={this.state.selectedValue}>
-          <option value="all">All</option>
-          <option value="hacked">Hacked</option>
-          <option value="accidentally published">Accidentally published</option>
-          <option value="vulnerability">Vulnerability</option>
-          <option value="leak">Leak</option>
-          <option value="poor security">Poor security</option>
-          <option value="configuration error">Configuration error</option>
-          <option value="inside job">Inside job</option>
-          <option value="lost / stolen computer">Lost / stolen computer</option>
-          <option value="lost / stolen media">Lost / stolen media</option>
-          <option value="inside job, hacked">Inside job, hacked</option>
-        </select>
-      </div>
+      <section className="_full-width-row query query_collapsed">
+        <div className="_container _container_large">
+          <div className="query--flex-container">
+            <div className="query--hack-type-label">
+              <label htmlFor="hack-type-select">What type of breach are you interested in?</label>
+            </div>
+            <div className="query--hack-type-select">
+              <select id="hack-type-select"
+                      onChange={this.handleChange}
+                      value={this.state.selectedValue}>
+                <option value="all">All</option>
+                <option value="hacked">Hacked</option>
+                <option value="accidentally published">Accidentally published</option>
+                <option value="vulnerability">Vulnerability</option>
+                <option value="leak">Leak</option>
+                <option value="poor security">Poor security</option>
+                <option value="configuration error">Configuration error</option>
+                <option value="inside job">Inside job</option>
+                <option value="lost / stolen computer">Lost / stolen computer</option>
+                <option value="lost / stolen media">Lost / stolen media</option>
+                <option value="inside job, hacked">Inside job, hacked</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 });
