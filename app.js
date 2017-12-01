35cc8ee7a5cc21641da6b683e9744ff8b679b4a5
Dolph Mathews <dolph.mathews@gmail.com>
2017-12-01 09:53:59 -0600

    Remove trailing whitespace

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /**
  * Copyright 2017 IBM Corp. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -58,7 +58,7 @@ discoverySetup.setupDiscovery(discoverySetupParams, (err, data) => {
     // we had to create them from scratch.
     environment_id = collectionParams.environment_id;
     collection_id = collectionParams.collection_id;
-    
+
     collectionParams.documents = discoveryDocs;
     console.log('Begin loading ' + discoveryDocs.length + ' json files into discovery. Please be patient as this can take several minutes.');
     discoverySetup.loadCollectionFiles(collectionParams);
