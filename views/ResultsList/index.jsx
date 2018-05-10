555cd55ebcf5db28326bb1830241146a70259981
Rich Hagarty <rich.hagarty@ibm.com>
2018-05-10 11:06:34 -0700

    added pagination

--- views/ResultsList/index.jsx
+++ views/ResultsList/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import QuerySyntax from '../QuerySyntax/index.jsx';
 import Detail from './Detail.jsx';
 import queryBuilder from '../../query-builder.js';  // eslint-disable-line
+import Pagination from 'react-js-pagination';
 
 export default React.createClass({
   displayName: 'ResultsList',
@@ -15,40 +16,75 @@ export default React.createClass({
   getInitialState() {
     return {
       showQuery: false,
+      currentPage: 1,
+      itemsPerPage: 15,
     };
   },
 
   onShowQuery() {
-    this.setState({ showQuery: true });
+    this.setState({
+      showQuery: true,
+      currentPage: 1,
+      itemsPerPage: 15
+    });
   },
 
   onShowResults() {
-    this.setState({ showQuery: false });
+    this.setState({
+      showQuery: false,
+      currentPage: 1,
+      itemsPerPage: 15
+    });
+  },
+
+  handlePageChange(pageNumber) {
+    this.setState({currentPage: pageNumber});
   },
 
   render() {
+    const indexOfLastItem = this.state.currentPage * this.state.itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - this.state.itemsPerPage;
+    const currentItems = this.props.items.slice(indexOfFirstItem, indexOfLastItem);
+
     return (
       <div>
         {!this.state.showQuery ? (
-          <div className="results-list widget">
-            <div className="widget--header">
-              <h2 className="base--h2 widget--header-title">
-                {this.props.items.length}
-                {this.props.items.length === 1 ? ' result' : ' results'}
-              </h2>
-              <div className="widget--header-spacer" />
-              <button
-                className="base--button widget--header-button"
-                href="#" onClick={this.onShowQuery}
-              >
-                View Query
-              </button>
+          <div>
+            <div className="results-list widget">
+              <div className="widget--header">
+                <h2 className="base--h2 widget--header-title">
+                  {this.props.items.length}
+                  {this.props.items.length === 1 ? ' result' : ' results'}
+                </h2>
+                <div className="widget--header-spacer" />
+                <button
+                  className="base--button widget--header-button"
+                  href="#" onClick={this.onShowQuery}
+                >
+                  View Query
+                </button>
+              </div>
+
+              <div className="widget--list">
+                {currentItems.map((item) =>
+                  <Detail key={item.id} item={item} />
+                )}
+              </div>
             </div>
 
-            <div className="widget--list">
-              {this.props.items.map((item) =>
-                <Detail key={item.id} item={item} />
-              )}
+            <div>
+              <Pagination
+                hideDisabled
+                prevPageText='prev'
+                nextPageText='next'
+                firstPageText='first'
+                lastPageText='last'
+                activePage={this.state.currentPage}
+                itemsCountPerPage={this.state.itemsPerPage}
+                totalItemsCount={this.props.items.length}
+                pageRangeDisplay={5}
+                onChange={this.handlePageChange}
+              />
             </div>
           </div>
         ) : (
